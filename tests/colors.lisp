c6aa485672af4f8e4f4995ab62b33ab52c158288
Violet white <alexwhite42000@gmail.com>
2019-09-15 18:37:47 -0400

    Added descriptions for the tests

--- tests/colors.lisp
+++ tests/colors.lisp
@@ -1,10 +1,14 @@
-(in-package img-genner/test)
+(in-package img-genner/tests)
 (use-package :img-genner)
 
 (deftest color-test
   (testing "Byte order"
-    (ok (equalp #(255 255 0) (get-color "Yellow")))
+    (ok (equalp #(255 255 0) (get-color "Yellow"))
+        "Byte order correct"
+        )
     )
   (testing "alpha"
-    (ok (equalp #(255 255 0 255) (get-color "Yellow" t))))
+    (ok (equalp #(255 255 0 255) (get-color "Yellow" t)))
+    "Alpha extension works :)"
+    )
   )
