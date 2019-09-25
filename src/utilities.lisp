43a00841c20eec1cf8a1b3a68b18e04b77d4f044
Violet white <alexwhite42000@gmail.com>
2019-09-25 17:44:44 -0400

    Wrote convexity test

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -16,4 +16,12 @@
           do(vector-push-extend i v))
     (vector-push-extend item v)
     v))
+(defun convex(a b c)
+  "returns true if the angle abc is convex"
+  (declare (type (simple-array single-float (2)) a b c))
+  (let ((a (sub-point a b))
+        (c (sub-point c b)))
+    (< (atan (- (aref a 1) (aref c 1))
+             (- (aref a 0) (aref c 0)))
+       pi)))
 (print (macroexpand '(with-array-items ((a 1 2)(b 2 3)) c (setf a 2 b 5))))
